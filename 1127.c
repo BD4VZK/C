4f4bd2a396523d36dbd7cc5d4d80ced19c1bc4ca
LekhaChem <lekha@riseup.net>
2022-03-11 15:34:38 +0800

    d

--- 1127.c
+++ 1127.c
@@ -1,19 +1,9 @@
 #include <stdio.h>
 #define PI 3.14
-main(void)
+int main(void)
 {
-    float r, h, C1, Sa, Sb, Va, Vb = 0.0;
+    float r, h = 0.0;
     scanf("%f %f", &r, &h);
-    printf("C1=%.2f\n
-            Sa=%.2f\n
-            Sb=%.2f\n
-            Va=%.2f\n
-            Vb=%.2f\n"
-            2 * PI * r, 
-            PI * r * r, 
-            PI * r * r * 4, 
-            PI * r * r * r * 4 / 3, 
-            PI * r * r * h
-            );
+    printf("C1=%.2f\nSa=%.2f\nSb=%.2f\nVa=%.2f\nVb=%.2f\n", 2 * PI * r, PI * r * r, PI * r * r * 4, PI * r * r * r * 4 / 3, PI * r * r * h);
     return 0;
 }
