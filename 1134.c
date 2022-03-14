4d5843aa786a45dcbfda249cc1a57392142be484
LekhaChem <lekha@riseup.net>
2022-03-14 11:01:03 +0800

    AC(2)

---
+++ 1134.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int gcd(int x, int y)
+{
+    int i, j = x > y ? x : y;
+    for (i = j; i > 0; i--)
+        if (x % i == 0 && y % i == 0)
+            return i;
+}
+int lcm(int x, int y)
+{
+    int i, j = x > y ? y : x;
+    for (i = j; i <= x * y; i++)
+        if (i % x == 0 && i % y == 0)
+            return i;
+}
+int main()
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d %d", gcd(a, b), lcm(a, b));
+    return 0;
+}
